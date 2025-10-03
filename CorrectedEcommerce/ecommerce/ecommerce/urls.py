a76051136eadfad41ebc1a1329fd33ef17fc69be
TheWanderingShop <wanderingshop24@gmail.com>
2025-10-03 11:53:35 +0200

    Added the corrected files to be used as the main
    I fixed the bugs in my code. It can now be used with new models.

---
+++ CorrectedEcommerce/ecommerce/ecommerce/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from core.views import index
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", index, name="index"),
+    path('', include('core.urls')),
+    path('products/', include('item.urls')),
+
+    
+    # path("theme1/", theme1, name="theme1"),
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
